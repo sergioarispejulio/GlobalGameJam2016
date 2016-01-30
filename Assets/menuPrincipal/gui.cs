fd6cba7bc7d3b9199f032af12903fd4c62ec7cb7
irvin373 <irvin373@gmail.com>
2016-01-30 13:33:19 -0400

    interface alpha

---
+++ Assets/menuPrincipal/gui.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gui : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+		void OnGUI() {
+			if (GUILayout.Button("Press Me"))
+				Debug.Log("Hello!");
+		GUILayout.TextArea ("War of Gods");
+		}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
