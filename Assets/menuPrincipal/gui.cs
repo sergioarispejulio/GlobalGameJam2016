40c751595552a12f6893ee7be97df55eca3aff70
irvin373 <irvin373@gmail.com>
2016-01-30 13:51:38 -0400

    interface alpha

--- Assets/menuPrincipal/gui.cs
+++ Assets/menuPrincipal/gui.cs
@@ -8,11 +8,15 @@ public class gui : MonoBehaviour {
 	
 	}
 
-		void OnGUI() {
-			if (GUILayout.Button("Press Me"))
-				Debug.Log("Hello!");
-		GUILayout.TextArea ("War of Gods");
-		}
+	void OnGUI() {
+		if (GUILayout.Button("Press Me"))
+			Debug.Log("Hello!");
+		/*GUILayout.TextArea (new Rect(10,10,200,200), "Recuerdo esos tiempos donde todo era mas simple adorabamos a aquellos dioses " +
+			"cuando las nuevas generaciones empezaron a dudar de su divinidad, ahi es donde todo empezo a ser mas complicado" +
+			"ahora los dioses, estan furiosos y empiezaron a mandar mountruos, muestras de su fuiria por si furia" +
+			"se rumora que existen personas que intentar combatirlas, espero que alguien detenga a aquellos dioses. la la la" +
+			"no sabia que escribir :P");*/
+	}
 	// Update is called once per frame
 	void Update () {
 	
